0a0bce89054c2cffa0a20e7efddad48b2e7f14a5
Your Name <osmanshakib@gmail.com>
2022-01-24 19:06:41 -0700

    Add show, confirm, status, error components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import './styles.scss'
-
+import './styles.scss';
+// import Header from "components/Appointment/Header";
+// import Empty from "components/Appointment/Empty";
+// import Show from "components/Appointment/Show";
 export default function Appointment(props) {
   return (<article className="appointment"></article>)
 }
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,11 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -143,6 +148,27 @@ storiesOf("DayListItem", module)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add("Header", () => <Header time="12pm" />)
     .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+    .add("Show", () => (
+      <Show 
+        student="Lydia Miller-Jone" 
+        interviewer={interviewer} 
+        onEdit={action("onEdit")} 
+        onDelete = {action("onDelete")} 
+      />))
+    .add("Confirm", () => (
+      <Confirm
+        message="Delete the appointment?"
+        onConfirm={action("onConfirm")}
+        onCancel={action("onCancel")}
+      />
+    ))
+    .add("Status", () => (<Status message="Deleting" />))
+    .add("Error", () => (
+      <Error 
+        message="Could not delete appointment."
+        onClose={action("onClose")}
+      />
+    ))
     
 
     
